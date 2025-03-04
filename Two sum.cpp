b011837e85fbbb6ff9de5933633bbbbb362c02a9
Sri Padma Chintha <144418506+sripadma-git@users.noreply.github.com>
2025-03-04 22:38:03 +0530

    Update Two sum.cpp

--- Two sum.cpp
+++ Two sum.cpp
@@ -1,3 +1,32 @@
+Better Approach(using Hashing): 
+
+Intuition: Basically, in the previous approach we selected one element and then searched for the other one using a loop. Here instead of using a loop, we will use the HashMap to check if the other element i.e. target-(selected element) exists. Thus we can trim down the time complexity of the problem.
+
+And for the second variant, we will store the element along will its index in the HashMap. Thus we can easily retrieve the index of the other element i.e. target-(selected element) without iterating the array.
+
+Approach:
+
+The steps are as follows:
+
+We will select the element of the array one by one using a loop(say i).
+Then we will check if the other required element(i.e. target-arr[i]) exists in the hashMap.
+If that element exists, then we will return “YES” for the first variant or we will return the current index i.e. i, and the index of the element found using map i.e. mp[target-arr[i]].
+If that element does not exist, then we will just store the current element in the hashMap along with its index. Because in the future, the current element might be a part of our answer.
+Finally, if we are out of the loop, that means there is no such pair whose sum is equal to the target. In this case, we will return either “NO” or {-1, -1} as per the variant of the question.
+Dry Run: Given array, nums = [2,3,1,4], target = 4
+
+Note: Here x denotes the currently selected element.
+
+
+Time Complexity: O(N), where N = size of the array.
+Reason: The loop runs N times in the worst case and searching in a hashmap takes O(1) generally. So the time complexity is O(N).
+
+Note: In the worst case(which rarely happens), the unordered_map takes O(N) to find an element. In that case, the time complexity will be O(N2). If we use map instead of unordered_map, the time complexity will be O(N* logN) as the map data structure takes logN time to find an element.
+
+Space Complexity: O(N) as we use the map data structure.
+
+    
+
 #include <bits/stdc++.h>
 using namespace std;
 
