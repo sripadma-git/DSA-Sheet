d57e82138d263e1a88585d53645a8151bec55dbe
Sri Padma Chintha <144418506+sripadma-git@users.noreply.github.com>
2025-03-06 23:04:55 +0530

    Update Two sum.cpp

--- Two sum.cpp
+++ Two sum.cpp
@@ -1,5 +1,26 @@
-Better Approach(using Hashing): 
+Problem Statement: Given an array of integers arr[] and an integer target.
 
+1st variant: Return YES if there exist two numbers such that their sum is equal to the target. Otherwise, return NO.
+
+2nd variant: Return indices of the two numbers such that their sum is equal to the target. Otherwise, we will return {-1, -1}.
+
+Note: You are not allowed to use the same element twice. Example: If the target is equal to 6 and num[1] = 3, then nums[1] + nums[1] = target is not a solution.
+
+Examples:
+
+Example 1:
+Input Format: N = 5, arr[] = {2,6,5,8,11}, target = 14
+Result: YES (for 1st variant)
+       [1, 3] (for 2nd variant)
+Explanation: arr[1] + arr[3] = 14. So, the answer is “YES” for the first variant and [1, 3] for 2nd variant.
+
+Example 2:
+Input Format: N = 5, arr[] = {2,6,5,8,11}, target = 15
+Result: NO (for 1st variant)
+	[-1, -1] (for 2nd variant)
+Explanation: There exist no such two numbers whose sum is equal to the target
+
+Better Approach(using Hashing): 
 Intuition: Basically, in the previous approach we selected one element and then searched for the other one using a loop. Here instead of using a loop, we will use the HashMap to check if the other element i.e. target-(selected element) exists. Thus we can trim down the time complexity of the problem.
 
 And for the second variant, we will store the element along will its index in the HashMap. Thus we can easily retrieve the index of the other element i.e. target-(selected element) without iterating the array.
