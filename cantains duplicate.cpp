4e1306e3e32a66e61da7121f211b694b52b0e0eb
Sri Padma Chintha <144418506+sripadma-git@users.noreply.github.com>
2025-03-06 23:02:57 +0530

    Update cantains duplicate.cpp

--- cantains duplicate.cpp
+++ cantains duplicate.cpp
@@ -1,3 +1,23 @@
+Problem Statement: Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+Example:
+
+Example 1:
+Input: nums = [1, 2, 3, 1]
+Output: true.
+Explanation: 1 appeared two times in the input array.
+
+Example 2: 
+Input: nums = [1, 2, 3, 4]
+Output: false
+Explanation: input array does not contain any duplicate number. 
+Solution:
+Disclaimer: Don't jump directly to the solution, try it out yourself first.
+
+Approach 1: we can easily find any duplicate in the array just by using two nested loops. The first loop will pick integers one by one from the array and the second nested loop will check if there exists any duplicate or not.
+
+Code:
+
 #include<bits/stdc++.h>
 using namespace std;
     
