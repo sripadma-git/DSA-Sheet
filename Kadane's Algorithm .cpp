b97323b4b90711f381d64b0751cb18b2ea2a7a7c
Sri Padma Chintha <144418506+sripadma-git@users.noreply.github.com>
2025-03-07 22:46:40 +0530

    Update Kadane's Algorithm .cpp

--- Kadane's Algorithm .cpp
+++ Kadane's Algorithm .cpp
@@ -1,4 +1,19 @@
+Algorithm / Intuition
+    
+Intuition:
+We will check the sum of every possible subarray and consider the maximum among them. To get every possible subarray sum, we will be using three nested loops. The first loops(say i and j) will iterate over every possible starting index and ending index of a subarray. Basically, in each iteration, the subarray range will be from index i to index j. Using another loop we will get the sum of the elements of the subarray [i…..j]. Among all values of the sum calculated, we will consider the maximum one.
 
+Approach:
+The steps are as follows:
+
+First, we will run a loop(say i) that will select every possible starting index of the subarray. The possible starting indices can vary from index 0 to index n-1(n = size of the array).
+Inside the loop, we will run another loop(say j) that will signify the ending index of the subarray. For every subarray starting from the index i, the possible ending index can vary from index i to n-1(n = size of the array).
+After that for each subarray starting from index i and ending at index j (i.e. arr[i….j]), we will run another loop to calculate the sum of all the elements(of that particular subarray).
+
+
+Note: We are selecting every possible subarray using two nested loops and for each of them, we add all its elements using another loop.
+
+    
 #include <bits/stdc++.h>
 using namespace std;
 
